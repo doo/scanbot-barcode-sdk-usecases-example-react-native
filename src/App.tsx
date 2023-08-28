713000d08f30a25ece03536f09449b7baecac48f
StefanKScanbot <stefankrzhovski@scanbot.io>
2023-08-28 11:18:15 +0200

    Removed native component example usecase

--- src/App.tsx
+++ src/App.tsx
@@ -9,12 +9,10 @@ import {PrimaryRoutesParamList, Screens} from './types';
 import {Colors} from 'react-native/Libraries/NewAppScreen';
 import ScanbotBarcodeSDK from 'react-native-scanbot-barcode-scanner-sdk';
 import {ResultsScreen} from './ResultsScreen';
-import {NativeComponentsSingleBarcodeScanning} from './NativeComponentsSingleBarcodeScanning';
 
 const ScreenTitles: Record<Screens, string> = {
   [Screens.ScannersList]: 'Scanbot Barcode SDK Example',
   [Screens.Results]: 'Results',
-  [Screens.NativeComponentBarcode]: 'Native Component Example',
 };
 
 const Stack = createNativeStackNavigator<PrimaryRoutesParamList>();
@@ -56,13 +54,6 @@ function App() {
               component={ResultsScreen}
               options={{
                 title: ScreenTitles.Results,
-              }}
-            />
-            <Stack.Screen
-              name={Screens.NativeComponentBarcode}
-              component={NativeComponentsSingleBarcodeScanning}
-              options={{
-                title: ScreenTitles.NativeComponentBarcode,
               }}
             />
           </Stack.Navigator>
